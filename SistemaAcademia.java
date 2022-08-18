f4ed04b0fcb41923c74e019f73ef4cf8be9f0006
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2022-08-18 00:08:50 -0300

    Concluído!

---
+++ SistemaAcademia.java
@@ -0,0 +1,29 @@
+package gymSystem;
+//SistemaAcademia
+import java.util.Scanner;
+import gymSystem.DadosAlune;
+
+public class SistemaAcademia {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		int op = 0;
+		DadosAlune c1primeiroAcesso = new DadosAlune();
+		DadosAlune c2login = new DadosAlune();
+		
+		System.out.println("***********************************");
+		System.out.println("BEM - VINDE! ");
+		System.out.println("***********************************");
+		System.out.println("Digite (1) para se cadastrar em nosso programa");
+		
+		op = leia.nextInt();
+		if (op == 1) {
+			c1primeiroAcesso.primeiroAcesso();
+		}
+		
+
+	}
+
+}
